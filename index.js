ca9fc12daa7d17dc2690daabd2de0b368f64ea2e
William Hwang <willhwang90@gmail.com>
2022-05-03 09:52:04 -0400

    commit tree 5f304959519e01b85732357ad8b480298d25ce4f

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your code here!
+const main = document.querySelector("#main");
+
+main.remove();
+
+
+const newHeader = document.createElement('h1');
+const body = document.querySelector('body');
+
+newHeader.setAttribute("id", "victory")
+
+newHeader.textContent = "Your name is the champion"
+console.log(body)
+
+
+
+
+body.append(newHeader);
+
+//const element = document.createElement("div");
+// document.body.append(element);
+// const ul = document.createElement("ul");
+// for (let i = 0; i < 3; i++){
+//     const li = document.createElement('li');
+//     li.textContent = (i + 1);
+//     ul.append(li);
+// }
+// element.append(ul);
